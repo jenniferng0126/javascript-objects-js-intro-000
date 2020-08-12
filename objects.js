5fab0392bbb76dff23bd586271bec812a2ee1268
Jennifer Ng <ka.man.jennifer.ng@gmail.com>
2020-08-12 03:01:45 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {'Sam Smith':'Stay with me'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign(playlist, {artistName:songTitle});
+  return Object.assign(playlist, {[artistName]:songTitle});
 }
